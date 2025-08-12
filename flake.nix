03ba18286332642b780e2979a3eadc3489a13151
moschramm <52012729+moschramm@users.noreply.github.com>
2025-08-12 07:09:33 +0200

    initial commit using cloud-init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Flake for k8s-workshop";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cdrtools
+        ];
+      };
+    });
+  };
+}
