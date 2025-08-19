876e98e42402388ab9df6f484175aecaeea09e73
moschramm <52012729+moschramm@users.noreply.github.com>
2025-08-19 13:34:50 +0200

    chore: improve dev environment setup

--- flake.nix
+++ flake.nix
@@ -1,24 +1,38 @@
 {
-  description = "Flake for k8s-workshop";
+  description = "An empty flake template that you can adapt to your own environment";
 
+  # Flake inputs
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
+  # Flake outputs
   outputs = inputs: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    # The systems supported for this flake
+    supportedSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+      "x86_64-darwin" # 64-bit Intel macOS
+      "aarch64-darwin" # 64-bit ARM macOS
+    ];
+
+    # Helper to provide system-specific attributes
     forEachSupportedSystem = f:
       inputs.nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
+          pkgs = import inputs.nixpkgs {inherit system;};
         });
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          cdrtools
-        ];
+        # The Nix packages provided in the environment
+        # Add any you need here
+        packages = with pkgs; [cdrtools git-filter-repo];
+
+        # Set any environment variables for your dev shell
+        env = {};
+
+        # Add any shell logic you want executed any time the environment is activated
+        shellHook = ''
+        '';
       };
     });
   };
